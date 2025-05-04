e2430c391fc375650898ce480608ab10645c8c7a
Diprobouthe <marinos.dimitris@gmail.com>
2025-05-05 00:09:34 +0300

    Temporary: create admin user on deploy

--- pfc_platform/build.sh
+++ pfc_platform/build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# Create admin user if it doesn't exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'pfcadmin123')" | python manage.py shell
