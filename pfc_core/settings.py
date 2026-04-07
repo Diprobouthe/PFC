107abecaf3783380aaf63b7b20561112b23430a7
Diprobouthe <your@email.com>
2026-04-07 13:18:16 +0300

    csrf change

--- pfc_core/settings.py
+++ pfc_core/settings.py
@@ -48,7 +48,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://localhost:8000',
     'http://127.0.0.1:8000',
     'https://127.0.0.1:8000',
-    'https://pfc.events'
+    'https://pfc.events',
     'https://www.pfc.events'
 ]
 
