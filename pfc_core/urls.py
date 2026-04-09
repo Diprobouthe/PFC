9b8863b3419cfdcdf1b20718a789f17f634e076b
Diprobouthe <your@email.com>
2026-04-09 23:46:36 +0300

    media update

--- pfc_core/urls.py
+++ pfc_core/urls.py
@@ -60,3 +60,12 @@ urlpatterns = [
 # Serve media files during development and production
 if settings.DEBUG or True:  # Force media serving for sandbox/production
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+from django.conf import settings
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
